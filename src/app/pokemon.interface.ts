917e392ebf7d6fdd0bde37efb1386de2e6d6493c
Jean Carlos <alinecaleffi97@gmail.com>
2022-04-01 17:21:00 -0300

    att arquivos

--- src/app/pokemon.interface.ts
+++ src/app/pokemon.interface.ts
@@ -1,13 +1,35 @@
-export interface PokemonInterface{
-    nome: string,
-    level: number,
-    tipo: string,
-    moveset: any[]
-}
-
-export class Pokemon implements PokemonInterface{
-    nome: string = " ";
+export interface PokemonInterface {
+    nome: string;
+    level: number;
+    tipo: tiposPokemon;
+    moveset: string[];
+  }
+  
+  export class Pokemon implements PokemonInterface {
+    nome: string = "";
     level: number = 0;
-    tipo: string = "Normal";
+    tipo: tiposPokemon = tiposPokemon.Normal;
     moveset: string[] = [];
-}
+  
+  }
+  
+  export enum tiposPokemon {
+    Normal = 'Normal',
+    Fire = 'Fire',
+    Water = 'Water',
+    Grass = 'Grass',
+    Electric = 'Electric',
+    Ice = 'Ice',
+    Fighting = 'Fighting',
+    Poison = 'Poison',
+    Ground = 'Ground',
+    Flying = 'Flying',
+    Psychic = 'Psychic',
+    Bug = 'Bug',
+    Rock = 'Rock',
+    Ghost = 'Ghost',
+    Dark = 'Dark',
+    Dragon = 'Dragon',
+    Steel = 'Steel',
+    Fairy = 'Fairy',
+  }
